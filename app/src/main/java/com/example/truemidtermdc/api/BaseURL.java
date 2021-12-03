351360cf9b79e38778e050f3b663a0d5f6cc6b6c
mark <benablomarkdwin@gmail.com>
2021-12-03 14:59:10 +0800

    first commit

---
+++ app/src/main/java/com/example/truemidtermdc/api/BaseURL.java
@@ -0,0 +1,8 @@
+package com.example.truemidtermdc.api;
+
+public class BaseURL {
+    public static String baseURL() {
+        return "http://192.168.1.8/web_1st_sem/shoesapi/public/api/";
+
+    }
+}
